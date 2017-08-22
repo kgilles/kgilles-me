78b288243fbca472e49a6c26feb99862d4df9de7
Kenny Gilles <kenny.gilles@klarna.com>
2017-08-22 15:20:25 -0400

    Fix tests

--- src/containers/Events/index.spec.jsx
+++ src/containers/Events/index.spec.jsx
@@ -19,13 +19,13 @@ describe('Containers | Events', () => {
   it('renders the Events container', () => {
     const { enzymeWrapper } = setup();
 
-    expect(enzymeWrapper.find('#event').nodes.length).toBe(1);
+    expect(enzymeWrapper.find('#events').nodes.length).toBe(1);
   });
 
   it('renders as many child components as the amount of years', () => {
     const { enzymeWrapper, props } = setup();
     const numYears = (props.endYear - props.startYear) + 1;
 
-    expect(enzymeWrapper.find('#event').node.props.children.length).toBe(numYears);
+    expect(enzymeWrapper.find('#events').node.props.children.length).toBe(numYears);
   });
 });
