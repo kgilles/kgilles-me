fdd8abbab0be43e56b798c93b4d1ba6457014548
Kenny Gilles <kenny.gilles@klarna.com>
2017-11-12 11:38:20 +0100

    Chrome headless instead of PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,11 @@
 const webpackConfig = require('./webpack.config.js');
 
+// Chrome
+const ChromiumRevision = require('puppeteer/package.json').puppeteer.chromium_revision;
+const Downloader = require('puppeteer/utils/ChromiumDownloader');
+const revisionInfo = Downloader.revisionInfo(Downloader.currentPlatform(), ChromiumRevision);
+process.env.CHROME_BIN = revisionInfo.executablePath;
+
 module.exports = function(config) {
   config.set({
     basePath: './',
@@ -12,6 +18,6 @@ module.exports = function(config) {
     webpackMiddleware: {
         noInfo: true
     },
-    browsers: ['PhantomJS']
+    browsers: ['ChromeHeadless']
   })
 };
