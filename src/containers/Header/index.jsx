9a282cd431a42a23dd16ea414ed76f29bc495652
Kenny Gilles <kenny.gilles@klarna.com>
2018-02-19 18:13:29 -0500

    First header image test

--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './index.scss';
+import './dala.jpg';
 
 const Header = () => (
   <header>
--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
       }]
     },
     {
-      test: /\.png$/,
+      test: /\.jpg$/,
       loader: 'file-loader?name=[name].[ext]'
     }]
   },
