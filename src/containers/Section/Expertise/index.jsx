e6094ba9d74eea59812c5995af156cc865d34b73
Kenny Gilles <kenny.gilles@klarna.com>
2018-02-19 22:29:05 -0500

    Use dark Competence blocks

--- src/containers/Section/Expertise/index.jsx
+++ src/containers/Section/Expertise/index.jsx
@@ -4,13 +4,13 @@ import styles from './index.scss';
 
 const SectionExpertise = () => (
   <div className={styles.expertise}>
-    <Competence color='red' title='QA'>
+    <Competence color='#2b2b2b' title='QA'>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
     </Competence>
-    <Competence color='green' title='Web Development'>
+    <Competence color='#4c4b4b' title='Web Development'>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
     </Competence>
-    <Competence color='blue' title='DevOps'>
+    <Competence color='#6d6d6d' title='DevOps'>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
     </Competence>
   </div>
