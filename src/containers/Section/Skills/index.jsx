385d185ef85b817884b084388a429d016cb97c96
Kenny Gilles <kenny.gilles@klarna.com>
2018-03-04 20:38:05 +0100

    Make Skills section responsive

--- src/containers/Section/Skills/index.jsx
+++ src/containers/Section/Skills/index.jsx
@@ -5,7 +5,7 @@ import Skill from '../../../components/Skill';
 const SectionSkills = () => (
   <div className={styles.skills}>
     <div className={`${styles.part} ${styles.left}`}>
-      <span>Since middle school I've been "learning by doing". I quickly pick up and learn new topics and in a short amount of time I'm able contribute on a high level.</span>
+      <span>Since middle school I've been "learning by doing". I quickly pick up and learn new topics and in a short amount of time I'm able to contribute on a high level.</span>
     </div>
     <div className={`${styles.part} ${styles.right}`}>
       <Skill title='Continous Integration'>
