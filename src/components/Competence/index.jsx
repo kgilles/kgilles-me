d8d9aaa07ca4f4d2c4fd7215cd30545825455023
Kenny Gilles <kenny.gilles@klarna.com>
2018-03-04 14:37:13 +0100

    Minor style changes

--- src/components/Competence/index.jsx
+++ src/components/Competence/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './index.scss';
 
 const Competence = ({ children, color, title }) => (
-  <div style={{ background: color }}>
+  <div className={styles.block} style={{ background: color }}>
     <div className={styles.title}>
       {title}
     </div>
