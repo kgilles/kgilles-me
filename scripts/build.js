12ad306f9d2246bc1ec3ee854e66b5d1b67d05e4
Kenny Gilles <kenny.gilles@klarna.com>
2019-01-06 12:59:01 -0500

    Generate favicons for all cases

--- scripts/build.js
+++ scripts/build.js
@@ -6,6 +6,19 @@ const path = require('path');
 const baseDir = process.cwd();
 const buildDir = path.join(baseDir, 'build');
 
+const faviconRelatedFiles = [
+  'android-chrome-192x192.png',
+  'android-chrome-256x256.png',
+  'apple-touch-icon.png',
+  'browserconfig.xml',
+  'favicon-16x16.png',
+  'favicon-32x32.png',
+  'favicon.ico',
+  'mstile-150x150.png',
+  'safari-pinned-tab.svg',
+  'site.webmanifest'
+];
+
 if (!fs.existsSync(buildDir)) {
   console.log('Build folder does not exist - Creating');
   fs.mkdirSync(buildDir);
@@ -49,7 +62,7 @@ const bundleStylesheetTag = '<link rel="stylesheet" type="text/css" href="bundle
 
 const builtHtml = htmlContent.split("\n").reduce((acc, line, ind, arr) => {
   if (!line.includes('stylesheet')) {
-    if (line.includes('.png')) {
+    if (line.includes('<img') && line.includes('.png')) {
       const imagePath = line.match(/src="(.*?.png)/)[1];
       const newPath = `images/${imagePath}`;
       console.log(`Replacing image path ${imagePath} with ${newPath}`);
@@ -74,20 +87,30 @@ const builtHtml = htmlContent.split("\n").reduce((acc, line, ind, arr) => {
 fs.writeFileSync(buildHtmlFile, builtHtml);
 
 // Images
+console.log("\n=== Copying images to build folder ===");
+
 const imageBuildDir = path.join(buildDir, 'images');
 
 if (!fs.existsSync(imageBuildDir)) {
-  console.log('Image build folder does not exist - Creating');
+  console.log("Image build folder does not exist - Creating\n");
   fs.mkdirSync(imageBuildDir);
 }
-
-console.log("\n=== Copying images to build folder ===");
 fs.readdirSync(baseDir).forEach((file) => {
-  if (file.endsWith('.jpg') || file.endsWith('.png')) {
+  if ((file.endsWith('.jpg') || file.endsWith('.png')) && !faviconRelatedFiles.includes(file)) {
     const newImagePath = path.join(imageBuildDir, file);
 
     console.log(`Copying ${file} to ${newImagePath}`);
 
     fs.copyFileSync(file, newImagePath);
   }
+});
+
+// Favicons
+console.log("\n=== Copying favicon related files to build folder ===");
+
+faviconRelatedFiles.forEach((file) => {
+  const newPath = path.join(buildDir, file);
+  console.log(`Copying ${file} to ${newPath}`);
+
+  fs.copyFileSync(file, newPath);
 });
