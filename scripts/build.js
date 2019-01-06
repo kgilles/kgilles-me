49f417a8d65d1d2a1f019928e9e738e41232b75c
Kenny Gilles <kenny.gilles@klarna.com>
2019-01-06 12:21:27 -0500

    Update build script

--- scripts/build.js
+++ scripts/build.js
@@ -25,7 +25,17 @@ fs.readdirSync(stylesDir).forEach((file, i, arr) => {
   const styleFile = path.join(stylesDir, file);
   stylesContents = stylesContents + fs.readFileSync(styleFile, 'utf8');
 });
-const trimmedStyles = stylesContents.replace(/\n/g, '');
+
+let trimmedStyles = stylesContents.replace(/\n/g, '');
+const imagesPaths = trimmedStyles.match(/'.*?\.jpg'/g);
+
+imagesPaths.forEach((imagePath) => {
+  const purePath = imagePath.replace(/'/g, '');
+  const newPath = `images/${purePath.replace('../', '')}`;
+  console.log(`Replacing image path ${purePath} with ${newPath}`);
+
+  trimmedStyles = trimmedStyles.replace(purePath, newPath);
+});
 
 fs.writeFileSync(buildStylesFile, trimmedStyles);
 
@@ -39,6 +49,14 @@ const bundleStylesheetTag = '<link rel="stylesheet" type="text/css" href="bundle
 
 const builtHtml = htmlContent.split("\n").reduce((acc, line, ind, arr) => {
   if (!line.includes('stylesheet')) {
+    if (line.includes('.png')) {
+      const imagePath = line.match(/src="(.*?.png)/)[1];
+      const newPath = `images/${imagePath}`;
+      console.log(`Replacing image path ${imagePath} with ${newPath}`);
+
+      const updatedLine = line.replace(imagePath, newPath);
+      return acc + updatedLine;
+    }
     return acc + (ind !== 0 ? "\n" : "") + line;
   } else if (!arr[ind + 1].includes('stylesheet')) {
     console.log(`Stripping stylesheet tag ${line}`);
@@ -47,6 +65,7 @@ const builtHtml = htmlContent.split("\n").reduce((acc, line, ind, arr) => {
     const indentation = line.match(/[\s]+/)[0];
     return acc + "\n" + indentation + bundleStylesheetTag;
   }
+
   console.log(`Stripping stylesheet tag ${line}`);
 
   return acc;
@@ -55,10 +74,17 @@ const builtHtml = htmlContent.split("\n").reduce((acc, line, ind, arr) => {
 fs.writeFileSync(buildHtmlFile, builtHtml);
 
 // Images
+const imageBuildDir = path.join(buildDir, 'images');
+
+if (!fs.existsSync(imageBuildDir)) {
+  console.log('Image build folder does not exist - Creating');
+  fs.mkdirSync(imageBuildDir);
+}
+
 console.log("\n=== Copying images to build folder ===");
 fs.readdirSync(baseDir).forEach((file) => {
   if (file.endsWith('.jpg') || file.endsWith('.png')) {
-    const newImagePath = path.join(buildDir, file);
+    const newImagePath = path.join(imageBuildDir, file);
 
     console.log(`Copying ${file} to ${newImagePath}`);
 
